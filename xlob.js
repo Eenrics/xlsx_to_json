2c1675d9895f5057da28b3b07b1a2c55d5c089e6
Ebenezer Exetie <ebenezeresh525@gmail.com>
2023-06-09 14:54:59 +0300

    backend done

--- xlob.js
+++ xlob.js
@@ -1,7 +1,61 @@
 const xlsx = require('node-xlsx');
 
-const express = require('express')
-const app = express()
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('xlsx_db', 'xlsx', 'xlsx', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+const Title = sequelize.define('title', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+const Subtitle = sequelize.define('subtitle', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+const Item = sequelize.define('item', {
+  item_id: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  unit: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  quantity: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  rate: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  amount: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  }
+});
+
+Title.hasMany(Subtitle);
+Subtitle.belongsTo(Title);
+
+Subtitle.hasMany(Item);
+Item.belongsTo(Subtitle);
+
+// sequelize.sync();
+
 
 let title = ''
 let subtitle = ''
@@ -48,16 +102,20 @@ const convert_to_db = async () => {
       console.log('*********THIS IS SECTION TITLE***********')
 
       if (title !== '') {
+
+        if (subtitle !== '') {
+            data.push({subtitle, content})
+            console.log("pushed to data", data.length)
+          }
+          subtitle = ''
+          content = []
+    
+
         GDB.push({title, data})
         console.log("pushed to db", GDB.length)
       }
       title = worksheets[0].data[i][1]
       data = []
-      
-    //   await Content.create({
-    //     section_title: worksheets[0].data[i][1],
-    //     content_type: 'section_title'
-    //   })
     }
 
     else if (worksheets[0].data[i].length === 2 && /\d\./.test(worksheets[0].data[i][1])) {
@@ -66,14 +124,15 @@ const convert_to_db = async () => {
       if (subtitle !== '') {
         data.push({subtitle, content})
         console.log("pushed to data", data.length)
-      } 
+      }
+      console.log('subtitle', subtitle)
+      console.log('title', title)
+
       subtitle = worksheets[0].data[i][1]
       content = []
-      
-    //   await Content.create({
-    //     subtitle: worksheets[0].data[i][1],
-    //     content_type: 'subtitle'
-    //   });
+
+      console.log('subtitle', subtitle)
+      console.log('title', title)
     }
 
     else if (worksheets[0].data[i].length === 2) {
@@ -89,22 +148,11 @@ const convert_to_db = async () => {
         amount: null,
     })
     console.log("pushed to content", content.length)
-      
-    //   await Content.create({
-    //     description: worksheets[0].data[i][1],
-    //     item_no: lastContentId,
-    //     content_type: 'description'
-    //   });
     }
 
-    // else if (worksheets[0].data[i].length === 6 && /total carried to summary/i.test(worksheets[0].data[i][1])) {
-    //   console.log('*********THIS IS SUMMARY***********')
-    //   await Content.create({
-    //     summary: worksheets[0].data[i][1],
-    //     total: worksheets[0].data[i][5],
-    //     content_type: 'summary'
-    //   });
-    // }
+    else if (worksheets[0].data[i].length === 6 && /total carried to summary/i.test(worksheets[0].data[i][1])) {
+      console.log('*********THIS IS SUMMARY***********')
+    }
 
     else {
       if (worksheets[0].data[i][0]) lastContentId = worksheets[0].data[i][0]
@@ -117,20 +165,11 @@ const convert_to_db = async () => {
             item_id: lastContentId,
             description: worksheets[0].data[i][1],
             unit: worksheets[0].data[i][2],
-            quantity: worksheets[0].data[i][3],
-            rate: parseFloat(worksheets[0].data[i][4]),
-            amount: worksheets[0].data[i][5] !== NaN ? parseFloat(worksheets[0].data[i][5]) : 0,
+            quantity: parseFloat(worksheets[0].data[i][3]).toFixed(2),
+            rate: parseFloat(worksheets[0].data[i][4]).toFixed(2),
+            amount: worksheets[0].data[i][5] !== NaN ? parseFloat(worksheets[0].data[i][5]).toFixed(2) : 0,
         })
         console.log("pushed to content", content.length)
-        // await Content.create({
-        //   item_no: lastContentId,
-        //   description: worksheets[0].data[i][1],
-        //   unit: worksheets[0].data[i][2],
-        //   quantity: worksheets[0].data[i][3] !== '-' ? parseInt(worksheets[0].data[i][3]) : 0,
-        //   rate: parseFloat(worksheets[0].data[i][4]),
-        //   amount: worksheets[0].data[i][5] !== NaN ? parseFloat(worksheets[0].data[i][5]) : 0,
-        //   content_type: 'content'
-        // });
       }
     }
   }
@@ -139,16 +178,62 @@ const convert_to_db = async () => {
   if (data.length !== 0 || title !== '') GDB.push({title, data}) 
 
   console.log('*********DATABASE POPULATED***********')
-  console.log(JSON.stringify(GDB))
+  console.log(JSON.stringify(GDB[0].data.length))
   console.log(GDB.length)
   return GDB
-//   process.exit()
 }
 
 convert_to_db();
 
+
+
+
+// (async () => {
+//     await sequelize.sync({ force: true }); // to drop and re-create tables
+//     for (const title of GDB) {
+//       const newTitle = await Title.create({ name: title.title });
+//       for (const subtitle of title.data) {
+//         const newSubtitle = await Subtitle.create({ name: subtitle.subtitle, titleId: newTitle.id });
+//         for (const content of subtitle.content) {
+//           await Item.create({
+//             item_id: content.item_id,
+//             description: content.description,
+//             unit: content.unit,
+//             quantity: content.quantity,
+//             rate: content.rate,
+//             amount: content.amount,
+//             subtitleId: newSubtitle.id
+//           });
+//         }
+//       }
+//     }
+//     console.log('Data inserted successfully');
+//   })();
+
+//   const DBT = [
+//     {
+//       title: 'A. something', //string
+//       data: [
+//           {
+//             subtitle: "1. something", //string
+//             content: [
+//               {
+//                   item_id: 1.1, //float
+//                   description: 'something', //string
+//                   unit: 'something', //string
+//                   quantity: 0, //integer
+//                   rate: 10.00, //float fixed 2
+//                   amount: 1000 // integer
+//               }
+//             ]
+//           }
+//       ]
+//     }
+//   ]
+
+const express = require('express')
+const app = express()
+
 app.get('/', (req, res) => res.send(JSON.stringify(GDB)))
 
-const port = process.env.PORT || 3000
-
-app.listen(port, () => console.log('server is running', port))
+app.listen(3000, () => console.log('server is running'))
